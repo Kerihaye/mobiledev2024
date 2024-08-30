c7bbd898f957c9fda91f2c4b94b32d7399372979
Kerihaye <shironeko050104@gmail.com>
2024-08-30 15:27:11 +0700

    lab3

--- README.md
+++
@@ -1,6 +0,0 @@
-USTH ICT Android Development 2024
-========================================
-
-* 22BI13146
-* Nguyen Dinh Hai
-* ICT class 1
---
+++ app/src/main/java/vn/edu/usth/usth_weather/ForecastFragment.java
@@ -0,0 +1,23 @@
+package vn.edu.usth.usth_weather;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class ForecastFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+
+        // Set the background color of the fragment
+        view.setBackgroundColor((Color.parseColor("#2000FF00")));
+
+        return view;
+    }
+}
