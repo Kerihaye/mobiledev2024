3858b8db2aa64de6fbd70347a8e6d4272a60eeb2
Kerihaye <shironeko050104@gmail.com>
2024-09-03 21:27:27 +0700

    lab 4

--- app/src/main/java/vn/edu/usth/usth_weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/usth_weather/ForecastFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 public class ForecastFragment extends Fragment {
@@ -15,8 +17,16 @@ public class ForecastFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
 
-        // Set the background color of the fragment
-        view.setBackgroundColor((Color.parseColor("#2000FF00")));
+        // Set the background color of the fragment (if not done in XML)
+        view.setBackgroundColor(Color.parseColor("#2000FF00"));
+
+        // Lấy tham chiếu tới các thành phần trong layout
+        TextView dayTextView = view.findViewById(R.id.textview_day);
+        ImageView weatherIconView = view.findViewById(R.id.imageview_weather_icon);
+
+        // Bạn có thể thiết lập văn bản hoặc hình ảnh động cho các thành phần này nếu cần
+        dayTextView.setText("Thursday");
+        weatherIconView.setImageResource(R.drawable.sun); // Sử dụng tên ảnh đúng trong thư mục drawable
 
         return view;
     }
