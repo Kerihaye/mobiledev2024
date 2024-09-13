6ba5b851cde8d6974011f522ad9b1748c7ff9951
Kerihaye <shironeko050104@gmail.com>
2024-09-14 00:47:39 +0700

    prac 8

--- app/src/main/java/vn/edu/usth/usth_weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/usth_weather/WeatherActivity.java
@@ -3,17 +3,16 @@ package vn.edu.usth.usth_weather;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends FragmentActivity {
     public static final String Command = "WeatherActivity";
@@ -21,31 +20,54 @@ public class WeatherActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_weather);
 
-        View headerView = findViewById(R.id.header);
-        TextView headerTitle = headerView.findViewById(R.id.header_title); // ID này phải trùng khớp với trong header.xml
-        headerTitle.setText("Weather Header");
-
+        // Set up window insets handling for edge-to-edge layout
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        // Find the TabLayout and ViewPager
         ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+        // Set up the ViewPager and its adapter
+        FragmentStatePagerAdapter pagerAdapter = new FragmentStatePagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
             @Override
             public Fragment getItem(int position) {
+                // Return different instance of WeatherAndForecastFragment based on position
                 return WeatherAndForecastFragment.newInstance("Param1_" + position, "Param2_" + position);
             }
 
             @Override
             public int getCount() {
+                // Number of pages (fragments) in the ViewPager (just duplicated for now)
                 return 3;
             }
-        });
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                // Return the title for each tab
+                switch (position) {
+                    case 0:
+                        return "Hanoi, Vietnam";
+                    case 1:
+                        return "Paris, France";
+                    case 2:
+                        return "Toulouse, France";
+                    default:
+                        return null;
+                }
+            }
+        };
+
+        // Set the adapter to the ViewPager
+        viewPager.setAdapter(pagerAdapter);
+
+        // Attach the TabLayout to the ViewPager
+        tabLayout.setupWithViewPager(viewPager);
 
         Log.i(Command, "onCreate");
     }
